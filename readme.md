edd7c09bb2ffdcb27643eb009ce2887322d45548
negima <sortidocorp@gmail.com>
2022-08-10 09:29:13 +0200

    Feat: shortcuts test

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # Chrome-ext
 
-Splink links by 20
+**In progress**
+
+Manage favorites to store them in folders automatically via shortcuts to read them later.
+
+### Build the ext with css in dist file
+```
+> npm run build
+```
+
+- Use Tailwindcss for display
--- src/background.js
+++ src/background.js
@@ -1,1 +1,26 @@
-console.log("test");
+console.log('test');
+
+chrome.commands.onCommand.addListener(async (command) => {
+    console.log(`Command "${command}" triggered`);
+
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+        let currentTab = tabs[0];
+        console.log(`tab "${currentTab}" triggered`);
+
+        chrome.bookmarks.create(
+            {
+                'title': 'hot'
+            },
+            newFolder => {
+                console.log(`added folder: "${newFolder.title}" triggered, "${currentTab.title}"`);
+                chrome.bookmarks.create(
+                    {
+                        'parentId': newFolder.id,
+                        'url': currentTab.url,
+                        'title': currentTab.title
+                    }
+                );
+            },
+        );
+    });
+});
